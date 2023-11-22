44244ce17bf8c839fc15035214ebd16d67ab0e5e
yudhamyn <yudha.myn@gmail.com>
2023-11-22 22:50:53 +0700

    web_func

--- main.py
+++ main.py
@@ -23,17 +23,6 @@ st.title("Aplikasi Prediksi Dataset")
 # UPLOAD DATASET
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # # To read file as bytes:
-    # bytes_data = uploaded_file.getvalue()
-    # # st.write(bytes_data)
-
-    # # To convert to a string based IO:
-    # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    # # st.write(stringio)
-
-    # # To read file as string:
-    # string_data = stringio.read()
-    # # st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
     file_type = uploaded_file.type
